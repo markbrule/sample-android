8f0bb38eece321067e9fa6242c48eea90b85ba23
Mark Brule <markbrule@gmail.com>
2017-07-04 19:26:31 -0700

    Add config activity

--- EnvigilantMobile/app/src/main/java/com/example/mark/envigilantmobile/MainActivity.java
+++ EnvigilantMobile/app/src/main/java/com/example/mark/envigilantmobile/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.mark.envigilantmobile;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,5 +50,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onConfigureButton(View view) {
+        Intent intent = new Intent(this, ConfigActivity.class);
+        startActivity(intent);
     }
 }
